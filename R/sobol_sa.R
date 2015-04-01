19cd76cf3102489bb3eba3c2350b62c2d690041a
johnnay <johnnay@thelonious.cas.vanderbilt.edu>
2015-04-01 17:16:51 -0500

    option to set cores for parallel

--- R/sobol_sa.R
+++ R/sobol_sa.R
@@ -13,13 +13,15 @@
 #'  \code{abm} function to specify what outcome measure to use.
 #'@param sample_count  Optional Numeric vector length one. Default is 100.
 #'@param constraints Optional Character vector that is either "none" or is using
-#'  only variable names that are specified in the input_values List argument.
+#'  only variable names that are specified in the input_values List argument. 
 #'  This character vector is evaluated in an environment created for the sampled
-#'  data on the variables, and its evaluation results in a Logical vector that
+#'  data on the variables, and its evaluation results in a Logical vector that 
 #'  that subsets sampled.
 #'@param sobol_nboot Optional Numeric vector length one. Default is 1000.
 #'@param iterations Optional numeric vector length one.
 #'@param parallel Optional logical vector length one. Default is FALSE.
+#'@param cores Optional Numeric vector length one. Default is
+#'  parallel::detectCores().
 #'@param verbose Optional logical vector.
 #'  
 #'@return Returns a sobol objects that can be plotted by functions
@@ -59,10 +61,13 @@ sobol_sa <- function(abm,
                      sobol_nboot = 1000, 
                      iterations = NULL,
                      parallel = FALSE,
+                     cores = NULL,
                      verbose = TRUE){
   
   # Get names of input factors:
   input_names <- names(input_values)
+  
+  if(parallel==TRUE & missing(cores)) cores <- parallel::detectCores()
   
   # Create two samples, removing samples violating constraints, until you have enough:
   input.sets.1 <- create_set(input_values, input_names, sample_count, constraints)
@@ -86,7 +91,7 @@ sobol_sa <- function(abm,
   if(verbose) cat("Starting simulations \n")
   # simulation results for input factor sets (as matrix)
   if (parallel) {
-    doParallel::registerDoParallel(cores = parallel::detectCores())
+    doParallel::registerDoParallel(cores = cores)
   } # without registering the backend the %dopar% should just run as %do%
   if (missing(iterations)){
     sobol_sim <- foreach::`%dopar%`(foreach::foreach(i=seq(nrow(sobol_aggregate$X)), .combine='c'), {

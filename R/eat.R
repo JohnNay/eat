c875e8d4f5347a32c63635c42546b65f59e4dbe4
JohnNay <john.j.nay@gmail.com>
2016-04-05 16:27:42 -0500

    Big change: moved out sa functionality.

--- R/eat.R
+++ R/eat.R
@@ -5,13 +5,6 @@
 #' \item \code{\link{cv_abm}}
 #' \item \code{\link{training}}
 #' }
-#' \code{eat}'s main functions for analyzing the models once
-#' we have settled on one or a few with good performance:
-#' \enumerate{
-#' \item \code{\link{create_sample}}
-#' \item \code{\link{sobol_sa}}
-#' \item \code{\link{pc_sa}}
-#' }
 #' 
 #' @docType package
 #' @name eat
